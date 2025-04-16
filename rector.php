a215f8bb08e03e3923ae50b5ca7378c946b38e3d
Francis Hilaire <Prometee@users.noreply.github.com>
2025-04-16 12:58:04 +0200

    Add rector set to automate PHPUnit upgrade

--- rector.php
+++ rector.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $config): void
-{
-    $config->sets([
-        LevelSetList::UP_TO_PHP_82
-    ]);
-};
+return RectorConfig::configure()
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82,
+        PHPUnitSetList::PHPUNIT_100,
+    ])
+;
