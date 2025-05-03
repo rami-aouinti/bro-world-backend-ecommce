42029a1a26f0f66c29478d4244f95ccfed9936ee
Jibé Barth <barth.jib@gmail.com>
2025-05-03 18:29:39 +0200

    feat(stimulus): Switch to a bundle named namespace
    Require stimulus controller through controllers.json
    Do not add them by default on webpack config for both admin/shop

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const SyliusAdmin = require('@sylius-ui/admin');
 const SyliusShop = require('@sylius-ui/shop');
 
-const adminConfig = SyliusAdmin.getWebpackConfig(path.resolve(__dirname));
-const shopConfig = SyliusShop.getWebpackConfig(path.resolve(__dirname));
+const adminConfig = SyliusAdmin._getInternalWebpackConfig(path.resolve(__dirname));
+const shopConfig = SyliusShop._getInternalWebpackConfig(path.resolve(__dirname));
 
 module.exports = [adminConfig, shopConfig];
