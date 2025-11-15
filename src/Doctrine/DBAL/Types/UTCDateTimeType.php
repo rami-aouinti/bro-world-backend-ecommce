e2765e48f0248ecb051600d8392b33866e1e5b72
Rami <rami.aouinti@gmail.com>
2025-11-15 20:02:20 +0100

    Fix UTC datetime type signature for Doctrine DBAL 4

---
+++ src/Doctrine/DBAL/Types/UTCDateTimeType.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\DBAL\Types;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateTimeType;
+
+final class UTCDateTimeType extends DateTimeType
+{
+    private static ?DateTimeZone $utcTimezone = null;
+
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): mixed
+    {
+        if ($value instanceof DateTimeInterface) {
+            if (!$value instanceof DateTimeImmutable) {
+                $value = DateTimeImmutable::createFromInterface($value);
+            }
+
+            $value = $value->setTimezone($this->getUtcTimezone());
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): ?DateTime
+    {
+        $datetime = parent::convertToPHPValue($value, $platform);
+
+        if ($datetime === null) {
+            return null;
+        }
+
+        if ($datetime instanceof DateTimeImmutable) {
+            $datetime = DateTime::createFromImmutable($datetime);
+        }
+
+        $datetime->setTimezone($this->getUtcTimezone());
+
+        return $datetime;
+    }
+
+    public function getName(): string
+    {
+        return 'utc_datetime';
+    }
+
+    private function getUtcTimezone(): DateTimeZone
+    {
+        if (self::$utcTimezone instanceof DateTimeZone) {
+            return self::$utcTimezone;
+        }
+
+        self::$utcTimezone = new DateTimeZone('UTC');
+
+        return self::$utcTimezone;
+    }
+}
